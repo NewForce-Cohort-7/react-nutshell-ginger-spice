34399813457770f030998956e4c0dae5eeefb3ac
Austin Davis <davis.austin1.1.c@gmail.com>
2023-04-25 10:17:32 -0400

    auth creation via screenshare with team

--- src/Nutshell.js
+++ src/Nutshell.js
@@ -1,13 +1,24 @@
+import { Login } from './components/Auth/Login';
+import { Register } from './components/Auth/Register';
+import { Authorized } from './components/Views/Authorized';
+import { ApplicationViews } from './components/Views/ApplicationViews';
+import { Route, Routes } from "react-router-dom"
 import './Nutshell.css';
 
 function Nutshell() {
-  return (
-    <div className="Dashboard">
-      <header className="App-header">
-        <h1>Welcome to Nutshell</h1>
-      </header>
-    </div>
-  );
+  return <Routes>
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
+
+  <Route path="*" element={
+    <Authorized>
+      <>
+        <ApplicationViews />
+      </>
+    </Authorized>
+
+  } />
+</Routes>
 }
 
 export default Nutshell;
